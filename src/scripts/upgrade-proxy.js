d7061c6e672c27cd9cb2c85a3466edd5595a9deb
Santiago Palladino <spalladino@gmail.com>
2018-05-07 18:16:29 -0300

    Upgrade multiple proxies with a single command
    Command upgrade-proxy will now upgrade all proxies for a given
    contract alias if no address is specified. Also, if no contract
    alias is set, and a --all flag is set, all contracts in the app
    will be upgraded to the new version.

--- src/scripts/upgrade-proxy.js
+++ src/scripts/upgrade-proxy.js
@@ -1,7 +1,10 @@
 import AppController from "../models/AppController";
 
-export default async function upgradeProxy({ contractAlias, proxyAddress, initMethod, initArgs, network, txParams = {}, packageFileName = null, networkFileName = null }) {
+export default async function upgradeProxy({ contractAlias, proxyAddress, initMethod, initArgs, network, all, txParams = {}, packageFileName = null, networkFileName = null }) {
+  if (!contractAlias && !all) {
+    throw new Error("Please choose a contract name to upgrade, or set --all to upgrade all proxies in the application")
+  }
   const appController = new AppController(packageFileName).onNetwork(network, txParams, networkFileName);
-  await appController.upgradeProxy(contractAlias, proxyAddress, initMethod, initArgs);
+  await appController.upgradeProxies(contractAlias, proxyAddress, initMethod, initArgs);
   appController.writeNetworkPackage();
 }
