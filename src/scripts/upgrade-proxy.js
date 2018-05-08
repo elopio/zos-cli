d8f205379cd69130438e6977e5f1a1e371bcdbb1
Santiago Palladino <spalladino@gmail.com>
2018-05-08 17:55:44 -0300

    Check modifications in contracts before add/upgrade proxy
    This PR adds to the network package.zos file the bytecode of the
    contract at the time it was deployed. It is then compared to the
    bytecode of the contract locally before creating or upgrading a
    proxy. If there was a change, then the command fails; this can be
    overridden via a --force flag to either command.

--- src/scripts/upgrade-proxy.js
+++ src/scripts/upgrade-proxy.js
@@ -1,7 +1,8 @@
 import AppController from "../models/AppController";
 
-export default async function upgradeProxy({ contractAlias, proxyAddress, initMethod, initArgs, network, txParams = {}, packageFileName = null, networkFileName = null }) {
+export default async function upgradeProxy({ contractAlias, proxyAddress, initMethod, initArgs, network, txParams = {}, packageFileName = null, networkFileName = null, force = false }) {
   const appController = new AppController(packageFileName).onNetwork(network, txParams, networkFileName);
+  await appController.checkContractsChanged(!force);
   await appController.upgradeProxy(contractAlias, proxyAddress, initMethod, initArgs);
   appController.writeNetworkPackage();
 }
